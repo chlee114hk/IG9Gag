af104d4e7864ffea277076416a549b1969f2cd0b
Vincent Lee <vincent.lee@onehouse.hk>
2016-06-08 18:12:07 +0800

    update redis settings for production

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -5,7 +5,11 @@ REDIS_CONFIG = YAML.load( File.open( Rails.root.join("config/redis.yml") ) ).sym
 dflt = REDIS_CONFIG[:default].symbolize_keys
 cnfg = dflt.merge(REDIS_CONFIG[Rails.env.to_sym].symbolize_keys) if REDIS_CONFIG[Rails.env.to_sym]
 
-$redis = Redis.new(cnfg)
+if ENV["REDISCLOUD_URL"]
+    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+else
+    $redis = Redis.new(cnfg)
+end
 Redis::Objects.redis = $redis
 #$redis_ns = Redis::Namespace.new(cnfg[:namespace], :redis => $redis) if cnfg[:namespace]
 
